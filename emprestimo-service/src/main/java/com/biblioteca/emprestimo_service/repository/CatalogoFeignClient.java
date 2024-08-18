aec04e9d1523adddcf141f43ef969b3947ae76be
jorge.donascimento <jorge.donascimento@apoio.agro.gov.br>
2024-08-18 09:49:46 -0300

    refactor: remover URLs fixas de FeignClients
    - remover URL fixa de ClienteFeignClient
    - remover URL fixa de CatalogoFeignClient
    - ajustar instância e porta do Eureka em application.yml dos serviços
    - adicionar gateway ao projeto IDEA

--- emprestimo-service/src/main/java/com/biblioteca/emprestimo_service/repository/CatalogoFeignClient.java
+++ emprestimo-service/src/main/java/com/biblioteca/emprestimo_service/repository/CatalogoFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name = "catalogo-service", url = "http://localhost:8080")
+@FeignClient(name = "catalogo-service")
 public interface CatalogoFeignClient {
 
     @GetMapping("/livros/{id}")
